1cf0b9be390cf245821eeb5a0cc4c2db38086b4f
lolcharles2 <85645338+lolcharles2@users.noreply.github.com>
2021-07-28 14:14:09 -0400

    Implemented state value network with action chosen via one step look-ahead.

--- state_value_network/state_value_network.py
+++ state_value_network/state_value_network.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import time
+import copy
+from collections import namedtuple, deque
+
 #% matplotlib notebook
 
 # GPU if available
@@ -158,7 +161,7 @@ class Tetris:
         """
         self.board = self.getBlankBoard()
         self.current_piece = self.getNewPiece()
-        return self.convertToFeatures()
+        return self.convertToFeatures(self.board)
 
     def isOnBoard(self, x, y):
         """
@@ -178,7 +181,7 @@ class Tetris:
     def getBlankBoard(self):
         return np.zeros((BOARD_WIDTH, BOARD_HEIGHT))
 
-    def isValidPosition(self, x, y, rotation):
+    def isValidPosition(self, board, piece, x, y, rotation):
         """
         Checks if a piece has a valid position on the board.
         Args:
@@ -195,22 +198,40 @@ class Tetris:
             If the piece has a valid position on the board.
 
         """
-        shape = self.current_piece
         for dx in range(TEMPLATE_WIDTH):
             for dy in range(TEMPLATE_HEIGHT):
-                template = PIECES[shape][rotation % len(PIECES[shape])]
+                template = PIECES[piece][rotation % len(PIECES[piece])]
                 if template[dy][dx] == 'O':
                     board_x_pos, board_y_pos = x + (dx - 2), y - (dy - 2)
-                    if not self.isOnBoard(board_x_pos, board_y_pos) or self.board[board_x_pos][board_y_pos]:
+                    if not self.isOnBoard(board_x_pos, board_y_pos) or board[board_x_pos][board_y_pos]:
                         return False
         return True
 
     def getNewPiece(self):
         return random.choice(list(PIECES.keys()))
 
-    def getNextState(self, action):
+    def findXYCoordinate(self, piece, action, board):
+        rotation = action % 4
+        left_margin, right_margin, top_margin = PIECES_MARGINS[piece][
+            rotation % len(PIECES_MARGINS[piece])]
+        x = max(left_margin, min(action // 4, BOARD_WIDTH - right_margin - 1))
+
+        # Finding y coordinate to place the piece
+        valid_y = None
+        flag = False
+        for y in range(BOARD_HEIGHT - top_margin - 1, -2, -1):
+            if self.isValidPosition(board, piece, x, y, rotation):
+                flag = True
+            else:
+                if flag:
+                    valid_y = y + 1
+                break
+
+        return x, valid_y, rotation
+
+    def transitionState(self, action):
         """
-        Returns the next state given the current action.
+        Returns the next state given the action.
         Args:
             action: int
                 An integer representing the action chosen.
@@ -223,37 +244,36 @@ class Tetris:
             and if the game has finished.
 
         """
-        rotation = action % 4
-        left_margin, right_margin, top_margin = PIECES_MARGINS[self.current_piece][
-            rotation % len(PIECES_MARGINS[self.current_piece])]
-        x = max(left_margin, min(action // 4, BOARD_WIDTH - right_margin - 1))
+        x, y, rotation = self.findXYCoordinate(self.current_piece, action, self.board)
 
-        # Finding y coordinate to place the piece
-        valid_y = None
-        flag = False
-        for y in range(BOARD_HEIGHT - top_margin - 1, -2, -1):
-            if self.isValidPosition(x, y, rotation):
-                flag = True
-            else:
-                if flag:
-                    valid_y = y + 1
-                break
-
-        if valid_y != None:
-            self.placeOnBoard(x, valid_y, rotation)
-
-            lines_cleared = self.clearLines()
-            #delta_r, delta_c = self.countHoles()
-            #reward = (lines_cleared**2/16 - delta_r/(BOARD_HEIGHT*BOARD_WIDTH) - delta_c/(BOARD_HEIGHT*BOARD_WIDTH))/2
-            reward = 0.1 + (lines_cleared+1) ** 2
+        if y != None:
+            self.board = self.placeOnBoard(self.current_piece, x, y, rotation, self.board)
 
             self.current_piece = self.getNewPiece()
-            next_state = self.convertToFeatures()
+            next_state = self.convertToFeatures(self.board)
+
+            self.board, lines_cleared = self.clearLines(self.board)
+            #delta_r, delta_c = self.countHoles(self.board)
+            #reward = lines_cleared**2 - delta_r - delta_c + 0.1
+            reward = 0.1 + (lines_cleared+1) ** 2
+
             return reward, next_state, False, lines_cleared
 
-        return 0, self.convertToFeatures(), True, 0
+        return -1, self.convertToFeatures(self.board), True, 0
 
-    def placeOnBoard(self, x, y, rotation):
+    def getAllNextStates(self):
+        data = []
+        for action in range(BOARD_WIDTH * 4):
+            temp_board = copy.deepcopy(self.board)
+            x, y, rotation = self.findXYCoordinate(self.current_piece, action, temp_board)
+            if y != None:
+                board = self.placeOnBoard(self.current_piece, x, y, rotation, temp_board)
+                #board, _ = self.clearLines(board)
+                features = self.convertToFeatures(board)
+                data.append((action, features))
+        return data
+
+    def placeOnBoard(self, piece, x, y, rotation, board):
         """
         Places the current piece on the board. Assumes that the piece
         is in a valid position.
@@ -267,14 +287,16 @@ class Tetris:
 
         Returns: None
         """
-        template = PIECES[self.current_piece][rotation % len(PIECES[self.current_piece])]
+        template = PIECES[piece][rotation % len(PIECES[piece])]
         for dx in range(TEMPLATE_WIDTH):
             for dy in range(TEMPLATE_HEIGHT):
                 if template[dy][dx] == 'O':
                     board_x_pos, board_y_pos = x + (dx - 2), y - (dy - 2)
-                    self.board[board_x_pos][board_y_pos] = 1
+                    board[board_x_pos][board_y_pos] = 1.0
 
-    def clearLines(self):
+        return board
+
+    def clearLines(self, board):
         """
         Removes completed lines from the board.
         Returns: int
@@ -284,23 +306,23 @@ class Tetris:
         lines_removed = 0
         y = 0  # start y at the bottom of the board
         while y < BOARD_HEIGHT:
-            if self.isCompleteLine(y):
+            if self.isCompleteLine(y, board):
                 # Remove the line and pull boxes down by one line.
                 for pull_down_Y in range(y, BOARD_HEIGHT - 1):
                     for x in range(BOARD_WIDTH):
-                        self.board[x][pull_down_Y] = self.board[x][pull_down_Y + 1]
+                        board[x][pull_down_Y] = board[x][pull_down_Y + 1]
                 # Set very top line to blank.
                 for x in range(BOARD_WIDTH):
-                    self.board[x][BOARD_HEIGHT - 1] = BLANK
+                    board[x][BOARD_HEIGHT - 1] = BLANK
                 lines_removed += 1
                 # Note on the next iteration of the loop, y is the same.
                 # This is so that if the line that was pulled down is also
                 # complete, it will be removed.
             else:
                 y += 1  # move on to check next row up
-        return lines_removed
+        return board, lines_removed
 
-    def isCompleteLine(self, y):
+    def isCompleteLine(self, y, board):
         """
         Checks if the line at height y is complete.
         Args:
@@ -312,10 +334,10 @@ class Tetris:
 
         """
         for x in range(BOARD_WIDTH):
-            if self.board[x][y] == 0.0: return False
+            if board[x][y] == 0.0: return False
         return True
 
-    def convertToFeatures(self):
+    def convertToFeatures(self, board):
         """
         Converts the current board position and falling piece to a
         list of features.
@@ -327,25 +349,12 @@ class Tetris:
             Torch tensor of the features described above. Values normalized to be between -1 and 1.
 
         """
-        features = torch.zeros(len(PIECES) + 2 * BOARD_WIDTH - 1)
+        delta_r, delta_c = self.countHoles(board)
+        max_height, bumpiness = self.scoreBumpiness(board)
 
-        # One hot vector for the current piece
-        features[PIECES_IND[self.current_piece]] = 1.0
+        return torch.tensor([[delta_r, delta_c, max_height, bumpiness]], dtype=torch.float32).to(device)
 
-        # Maximum heights of each column
-        for x in range(BOARD_WIDTH):
-            for y in range(BOARD_HEIGHT - 1, -1, -1):
-                if self.board[x][y]:
-                    features[len(PIECES) + x] = y / BOARD_HEIGHT
-                    break
-
-        # Differences in heights between each column
-        for x in range(BOARD_WIDTH - 1):
-            features[len(PIECES) + BOARD_WIDTH + x] = features[len(PIECES) + x + 1] - features[len(PIECES) + x]
-
-        return features.to(device)
-
-    def countHoles(self):
+    def countHoles(self, board):
         """
         Counts the number of transitions from filled to empty or vice
         versa in the rows and columns.
@@ -358,25 +367,70 @@ class Tetris:
         delta_r = 0
         for y in range(BOARD_HEIGHT):
             for x in range(BOARD_WIDTH - 1):
-                if self.board[x][y] != self.board[x + 1][y]:
+                if board[x][y] != board[x + 1][y]:
                     delta_r += 1
 
         # Across columns:
         delta_c = 0
         for x in range(BOARD_WIDTH):
             for y in range(BOARD_HEIGHT - 1):
-                if self.board[x][y] != self.board[x][y + 1]:
+                if board[x][y] != board[x][y + 1]:
                     delta_c += 1
 
         return delta_r, delta_c
 
+    def scoreBumpiness(self, board):
+        heights = []
+
+        for x in range(BOARD_WIDTH):
+            for y in range(BOARD_HEIGHT-1, -1, -1):
+                if board[x][y]: break
+            heights.append(y)
+
+
+        diffs = [abs(heights[i]-heights[i-1]) for i in range(1, len(heights))]
+        diffs.sort()
+
+        return max(heights), sum(diffs)-diffs[-1]
+
+Transition = namedtuple('Transition', ('state', 'next_state', 'reward', 'done'))
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        """
+        Initializes the replay memory.
+        Args:
+            capacity: int
+                The capacity of the memory.
+        """
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Saves a transition.
+        """
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        """
+        Samples a batch of transitions of size batch_size
+        randomly.
+        Args:
+            batch_size: int
+                The size of the batch.
+        Returns: list[tuple]
+            A list of the sampled transitions.
+        """
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
 class Agent:
     """ Agent object that uses the actor-critic network to find the
         optimal policy.
     """
 
-    def __init__(self, env, NN):
+    def __init__(self, env, NN, optimizer, criterion):
         """ Initializes the agent.
 
             @type env: Tetris
@@ -388,11 +442,15 @@ class Agent:
         """
         self.env = env
         self.NN = NN
+        self.optimizer = optimizer
+        self.criterion = criterion
 
-    def chooseAction(self, state, epsilon):
-        """ Chooses action according to the current policy.
-            @type state: torch tensor
-                A torch tensor representing the current state.
+    def chooseAction(self, epsilon):
+        """ Chooses action. With probability epsilon it will choose
+            an exploratory action. Otherwise, it will choose
+            an action which maximizes the estimated reward of all
+            possible next states.
+
             @type epsilon: float
                 Exploration probability.
             @rtype: int
@@ -401,59 +459,43 @@ class Agent:
         if random.uniform(0, 1) < epsilon:
             return random.randrange(BOARD_WIDTH * 4)
 
-        with torch.no_grad():
-            return int(torch.argmax(self.NN(state)))
+        cur_best_val = -float('inf')
+        cur_best_action = 0
 
-    def initializeTrace(self):
-        """
-        Initializes the eligibility trace for the value network.
-        @rtype: dict
-            A dictionary that maps the layers of the value network
-            to their eligibility traces.
-        """
-        z_w = {}
-        with torch.no_grad():
-            for p in self.NN.parameters():
-                z_w[p] = torch.zeros(size=p.data.size()).to(device)
+        data = env.getAllNextStates()
 
-        return z_w
+        for action, state in data:
+            value = self.NN(state).item()
+            if value > cur_best_val:
+                cur_best_val = value
+                cur_best_action = action
 
+        return cur_best_action
 
-    def oneStep(self, state, action, delta, gamma, z, alpha, lmbda):
-        """ Performs one training step on observed transition.
-            @type state: torch tensor
-                A torch tensor denoting the position.
-            @type action: int
-                An integer representing the action chosen.
-            @type delta: float
-                The TD error.
-            @type gamma: float
-                The discount factor.
-            @type z: dict
-                A dictionary that maps value model parameters to their
-                eligibility traces.
-            @type alpha: float
-                The step size.
-            @type lmbda: float
-                Trace decay parameter.
-            @rtype: dict
-                A dictionary denoting the updated eligibility trace dictionary.
-        """
-        pred = self.NN(state)
+    def optimizeModel(self, memory, batch_size, gamma):
+        if len(memory) >= batch_size:
+            transitions = memory.sample(batch_size)
+            batch = Transition(*zip(*transitions))
 
-        self.NN.zero_grad()
+            state_batch = torch.cat(batch.state)
+            next_state_batch = torch.cat(batch.next_state)
+            reward_batch = torch.cat(batch.reward)
+            done_batch = torch.cat(batch.done)
 
-        pred[action].backward()
+            predictions = self.NN(state_batch)
 
-        # Update eligibility trace and parameters
-        with torch.no_grad():
-            for p in self.NN.parameters():
-                z[p] = gamma * lmbda * z[p] + p.grad
-                p.copy_(p + alpha * delta * z[p])
+            with torch.no_grad():
+                targets = reward_batch + gamma * self.NN(next_state_batch) * done_batch
 
-        return z
+            loss = self.criterion(predictions, targets)
 
-    def train(self, episodes, epsilon, gamma, alpha, lmbda):
+            loss.backward()
+
+            self.optimizer.step()
+
+            self.optimizer.zero_grad()
+
+    def train(self, episodes, epsilon, gamma, memory_capacity, batch_size):
         """ Trains the agent using the actor-critic method with eligibility traces.
 
             @type episodes: int
@@ -477,27 +519,26 @@ class Agent:
         #plt.xlim(-0.5, BOARD_WIDTH - 0.5)
         #plt.ylim(BOARD_HEIGHT - 0.5, -0.5)
 
+        memory = ReplayMemory(memory_capacity)
+
         tot_steps = 0
 
         LC = 0
 
         for episode in range(episodes):
 
-            if (episode + 1) % 100 == 0:
+            if (episode + 1) % 10 == 0:
                 print(f'Episode {episode + 1}/{episodes} completed!')
                 torch.save(self.NN.state_dict(), 'tetris_NN_value_model')
-                print(f'Average steps per episode: {tot_steps / 100}')
-                print(f'Average lines cleared per episode: {LC / 100}')
+                print(f'Average steps per episode: {tot_steps / 10}')
+                print(f'Average lines cleared per episode: {LC / 10}')
 
                 tot_steps = 0
                 LC = 0
 
             state, done = self.env.reset(), False
 
-            action = self.chooseAction(state, epsilon)
-
             # Initialize eligibility traces
-            z = self.initializeTrace()
 
             while not done:
                 tot_steps += 1
@@ -509,27 +550,29 @@ class Agent:
                 #ax.set_yticks(np.arange(0.5, BOARD_HEIGHT - 0.5, 1))
                 #fig.canvas.draw()
 
-                reward, next_state, done, lines_cleared = self.env.getNextState(action)
+                action = self.chooseAction(epsilon)
 
-                next_action = self.chooseAction(next_state, epsilon)
+                reward, next_state, done, lines_cleared = self.env.transitionState(action)
+                reward = torch.tensor([[reward]], device=device)
+                done = torch.tensor([[done]], device=device)
+
+                # Saves the transition
+                memory.push(state, next_state, reward, done)
 
                 LC += lines_cleared
 
-                # TD error
-                target = reward + gamma * self.NN(next_state)[next_action] if not done else reward
-                delta = target - self.NN(state)[action]
+                # Perform one step of batch gradient descent
+                self.optimizeModel(memory, batch_size, gamma)
 
-                z = self.oneStep(state, action, delta, gamma, z, alpha, lmbda)
-
-                state, action = next_state, next_action
+                state = next_state
 
 
 class QNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2, action_size):
+    def __init__(self, input_size, hidden_size1, hidden_size2):
         super(QNetwork, self).__init__()
         self.l1 = nn.Linear(input_size, hidden_size1)
         self.l2 = nn.Linear(hidden_size1, hidden_size2)
-        self.l3 = nn.Linear(hidden_size2, action_size)
+        self.l3 = nn.Linear(hidden_size2, 1)
 
     def forward(self, x):
         x = torch.tanh(self.l1(x))
@@ -541,23 +584,25 @@ class QNetwork(nn.Module):
 
 if __name__ == "__main__":
     # Network parameters
-    input_size = len(PIECES) + 2 * BOARD_WIDTH - 1
-    action_size = BOARD_WIDTH * 4
+    input_size = 4
     hidden_size1 = 50
     hidden_size2 = 40
 
     # Training parameters
     episodes = 1000000
-    gamma = 0.99
-    alpha = 4e-4
-    lmbda = 0.85
+    gamma = 0.95
+    learning_rate = 3e-4
     epsilon = 0.1
+    memory_capacity = 1000
+    batch_size = 16
 
     env = Tetris()
-    model_value = QNetwork(input_size, hidden_size1, hidden_size2, action_size).to(device)
+    model_value = QNetwork(input_size, hidden_size1, hidden_size2).to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(model_value.parameters(), lr=learning_rate)
 
-    model_value.load_state_dict(torch.load('tetris_NN_value_model'))
+    #model_value.load_state_dict(torch.load('tetris_NN_value_model'))
 
-    tetris_agent = Agent(env, model_value)
+    tetris_agent = Agent(env, model_value, optimizer, criterion)
 
-    tetris_agent.train(episodes, epsilon, gamma, alpha, lmbda)
+    tetris_agent.train(episodes, epsilon, gamma, memory_capacity, batch_size)
